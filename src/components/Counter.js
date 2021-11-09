8fed07fe734bb10d8f018d38dfccac7975949be0
gekoparty <gekoparty@hotmail.com>
2021-11-09 18:18:56 +0100

    Added +5 counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -17,6 +17,13 @@ const Counter = () => {
     });
   };
 
+  const increaseHandler = () => {
+    dispatch({
+      type: 'increase',
+      amount: 5
+    });
+  }
+
   const toggleCounterHandler = () => {};
 
   return (
@@ -25,6 +32,7 @@ const Counter = () => {
       <div className={classes.value}>{counter}</div>
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
