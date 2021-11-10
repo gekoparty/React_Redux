e2a984e9a4c9e0a89b5289f4b4d38e8b43f08083
gekoparty <gekoparty@hotmail.com>
2021-11-10 18:11:03 +0100

    multiple slices

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,24 @@
 import classes from './Header.module.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { authActions } from '../store';
+
+
 
 const Header = () => {
+
+  const dispatch = useDispatch();
+  const isAuth = useSelector(state => state.auth.isAuthenticated);
+
+  const logoutHandler = (event) => {
+    event.preventDefault();
+
+    dispatch(authActions.logout());
+  }
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
+      {isAuth && (
       <nav>
         <ul>
           <li>
@@ -13,10 +28,11 @@ const Header = () => {
             <a href='/'>My Sales</a>
           </li>
           <li>
-            <button>Logout</button>
+            <button onClick={logoutHandler}>Logout</button>
           </li>
         </ul>
       </nav>
+      )}
     </header>
   );
 };
