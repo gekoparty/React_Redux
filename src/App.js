00292acd8ba738ee0929364bcc2f0e7b02c26eb7
gekoparty <gekoparty@hotmail.com>
2021-11-17 17:48:44 +0100

    added NavLink

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { Route } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import MainHeader from "./components/MainHeader";
+
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <MainHeader />
+      <Route path="/Welcome">
+        <Welcome />
+      </Route>
+      <Route path="/Products">
+        <Products />
+      </Route>
     </div>
   );
 }
