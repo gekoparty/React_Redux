f05366e92297723f79a5651e85e659c82233a513
gekoparty <gekoparty@hotmail.com>
2021-12-16 20:49:29 +0100

    finished

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
 import MainHeader from "./components/MainHeader";
@@ -10,15 +10,18 @@ function App() {
       <MainHeader />
       <main>
         <Switch>
-        <Route path="/welcome">
-          <Welcome />
-        </Route>
-        <Route path="/products" exact>
-          <Products />
-        </Route>
-        <Route path="/products/:productId">
-          <ProductDetail />
-        </Route>
+          <Route path="/" exact>
+            <Redirect to="/welcome"/>
+            </Route>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetail />
+          </Route>
         </Switch>
       </main>
     </div>
